d042d035d9b0d8e9c93581cb19bd7dcc29a54a89
Wjl <337629662@qq.com>
2018-01-04 19:46:44 +0800

    添加子路由

--- demo2/src/main.js
+++ demo2/src/main.js
@@ -2,14 +2,44 @@ import Vue from 'vue'
 import Layout from './components/layout.vue'
 import VueRouter from 'vue-router'
 import IndexPage from './pages/index'
+import DetailPage from './pages/detail.vue'
+import DetailAnaPage from './pages/detail/analysis.vue'
+import DetailCouPage from './pages/detail/count.vue'
+import DetailForPage from './pages/detail/forecast.vue'
+import DetailPubPage from './pages/detail/publish.vue'
 Vue.use(VueRouter)
 
 let router = new VueRouter({
   mode: 'history',
-  routes: [{
-    path: '/',
-    component: IndexPage
-  }]
+  routes: [
+    {
+      path: '/',
+      component: IndexPage
+    },
+    {
+      path:'/detail',
+      component:DetailPage,
+      redirect:'/detail/analysis',
+      children:[
+        {
+          path:'analysis',
+          component:DetailAnaPage
+        },
+        {
+          path:'count',
+          component:DetailCouPage
+        },
+        {
+          path:'forecast',
+          component:DetailForPage
+        },
+        {
+          path:'publish',
+          component:DetailPubPage
+        },
+      ]
+    }
+  ]
 
 })
 new Vue({
