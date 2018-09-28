6f1a6c365c20c88b5b1e06c1df5a0a94dee6f483
Wjl <337629662@qq.com>
2018-09-28 17:51:38 +0800

    播放器组件基础样式，基础数据展示

--- musicPlayer/vue-music/src/store/getters.js
+++ musicPlayer/vue-music/src/store/getters.js
@@ -9,5 +9,5 @@ export const mode = state => state.mode
 export const currentIndex = state => state.currentIndex
 
 export const currentSong = (state) => {
-  return state.playList[state.currentIndex] || {}
+  return state.playlist[state.currentIndex] || {}
 }
